1f58aa16f30012215b6075b604024285ffde0f83
d7q0b <aliceykim0828@gmail.com>
2019-05-23 11:03:18 -0700

    Add CSS and icons to profile page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,14 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { FooterComponent } from './components/footer/footer.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { CalendarComponent } from './components/calendar/calendar.component';
+import { FormsModule } from '@angular/forms';
+import { FlatpickrModule } from 'angularx-flatpickr';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -25,12 +33,23 @@ import { ProfileComponent } from './components/profile/profile.component';
     ConfirmationComponent,
     NavbarComponent,
     FooterComponent,
-    ProfileComponent
+    ProfileComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AngularFontAwesomeModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    FormsModule,
+    NgbModalModule,
+    FlatpickrModule.forRoot(),
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory
+    }),
+    FormsModule,
     RouterModule.forRoot([
       {
         path: '',
