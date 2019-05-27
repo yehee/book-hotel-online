b77b4d59acb782b11517b0d335b6f4f731cd2e4a
d7q0b <aliceykim0828@gmail.com>
2019-05-27 16:33:34 -0700

    Implement all functionality related to reviews except editing

--- src/app/components/analytics/analytics.component.ts
+++ src/app/components/analytics/analytics.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Chart } from 'angular-highcharts';
 import { ReservationService } from 'src/app/services/reservation.service';
+import { ReviewService } from 'src/app/services/review.service';
 
 @Component({
   selector: 'app-analytics',
@@ -9,9 +10,9 @@ import { ReservationService } from 'src/app/services/reservation.service';
 })
 
 export class AnalyticsComponent implements OnInit {
-  chart;
+  chart; rating; reviews = [];
 
-  constructor(private reservationService : ReservationService) {
+  constructor(private reservationService : ReservationService, private reviewService : ReviewService) {
     this.chart = new Chart({
       chart: {
         type: 'line',
@@ -42,10 +43,61 @@ export class AnalyticsComponent implements OnInit {
         }
       ]
     });
+    this.rating = new Chart({
+      chart: {
+          plotBackgroundColor: null,
+          plotBorderWidth: null,
+          plotShadow: false,
+          type: 'pie'
+      },
+      colors: ['#2a4d69', '#4b86b4', '#adcbe3', '#e7eff6', '#63ace5',
+               '#492970', '#f28f43', '#77a1e5', '#c42525', '#a6c96a'],
+      title: {
+          text: null
+      },
+      tooltip: {
+          pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+      },
+      plotOptions: {
+          pie: {
+              allowPointSelect: true,
+              cursor: 'pointer',
+              dataLabels: {
+                  enabled: false
+              },
+              showInLegend: true,
+              slicedOffset: 0
+          }
+      },
+      series: [{
+        name: 'Brands',
+        type: 'pie',
+        colorByPoint: true,
+        data: [{
+          name: '5',
+          y: 61.41,
+          sliced: true,
+          selected: true
+        }, {
+          name: '4',
+          y: 17.9
+        }, {
+          name: '3',
+          y: 11.84
+        }, {
+          name: '2',
+          y: 4.67
+        }, {
+          name: '1',
+          y: 4.18
+        }]
+      }]
+    });
   }
 
   ngOnInit() {
     this.update();
+    this.getReviews();
   }
 
   update() {
@@ -64,4 +116,15 @@ export class AnalyticsComponent implements OnInit {
     this.chart.addPoint(count);
   }
 
+  getReviews(): void {
+    this.reviewService.getReviews().subscribe(
+      (res: any[]) => {
+        console.log(res);
+        this.reviews = res['data'];
+      },
+      (err) => {
+        console.error(err);
+      }
+    );
+  }
 }
